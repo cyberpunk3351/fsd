51ebf44abe1c059abeb2f8b4591d7e98cedf357c
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2019-11-21 14:30:54 +0700

    start project. add folder folder structure.

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp            = require('gulp'); 
+var browserSync     = require('browser-sync').create();
+var sass            = require('gulp-sass');
+var notify          = require("gulp-notify");
+var pug 		    = require('gulp-pug');
+var rename 		    = require('gulp-rename');
+var clean           = require('gulp-clean');
+var autoprefixer 	= require('gulp-autoprefixer');
+var gcmq 	  	 	= require('gulp-group-css-media-queries');
+
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+	});
+});
+
+gulp.task('styles', function() {
+	// return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	return gulp.src('src/sass/**/*.sass')
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 5 versions']))
+	// .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('src/css')) // папака в которую складывают уже готовые css стили
+	.pipe(browserSync.stream())
+});
+
+gulp.task('pug', function(){
+    return gulp.src(['src/pug/*.pug','!src/pug/_layouts/*.*'])
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('gcmq', async function () {
+    gulp.src('src/css/main.gcmq.css')
+        .pipe(gcmq())
+        .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('copy-img', function() {
+    return gulp.src('./src/img/*.svg')
+        .pipe(gulp.dest('./dist/imgs'));
+});
+
+gulp.task('copy-font', function() {
+    return gulp.src('./src/font/**/*.{woff,woff2,eof}')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+
+gulp.task('rename', function() {
+    return gulp.src('src/css/main.gcmq.css')
+    .pipe(rename("main.css"))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src('./dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['src/sass/**/*.sass', './src/pug/modules/**/*.sass'], gulp.parallel('styles'));
+    gulp.watch(['src/css/main.min.css'], gulp.parallel('gcmq'));
+	gulp.watch('src/pug/**/*.pug', gulp.parallel('pug'));
+});
+
+gulp.task('default', gulp.parallel('styles', 'gcmq', 'rename', 'pug', 'browser-sync', 'watch'));
+gulp.task('copy', gulp.parallel('copy-img', 'copy-font'));
